601237ee4680d03a3a2e04bf9e8dcf4837227abe
Andrew <atitoia@wgu.edu>
2024-11-11 10:52:44 -0800

    Added user id handling into the app component

--- frontend/remission-frontend/src/app/app.component.ts
+++ frontend/remission-frontend/src/app/app.component.ts
@@ -17,19 +17,44 @@ export class AppComponent implements OnInit {
     this.initializeUserId();
   }
 
+  /**
+   * Initializes the user ID for the current session.
+   * Checks localStorage for an existing ID or generates a new one.
+   */
   initializeUserId(): void {
-    const storedUserId = localStorage.getItem('user_id');
-    if (!storedUserId) {
-      this.userId = this.generateRandomUserId();
-      localStorage.setItem('user_id', this.userId.toString());
-      console.log(`New User ID Generated: ${this.userId}`);
-    } else {
-      this.userId = parseInt(storedUserId, 10);
+    const storedUserId = this.getUserIdFromLocalStorage();
+    if (storedUserId) {
+      this.userId = storedUserId;
       console.log(`Existing User ID: ${this.userId}`);
+    } else {
+      this.userId = this.generateRandomUserId();
+      this.storeUserIdInLocalStorage(this.userId);
+      console.log(`New User ID Generated: ${this.userId}`);
     }
   }
 
+  /**
+   * Retrieves the user ID from localStorage.
+   * @returns {number | null} The stored user ID or null if not found.
+   */
+  getUserIdFromLocalStorage(): number | null {
+    const userId = localStorage.getItem('user_id');
+    return userId ? parseInt(userId, 10) : null;
+  }
+
+  /**
+   * Stores the generated user ID in localStorage.
+   * @param userId The user ID to store.
+   */
+  storeUserIdInLocalStorage(userId: number): void {
+    localStorage.setItem('user_id', userId.toString());
+  }
+
+  /**
+   * Generates a random 5-digit user ID.
+   * @returns {number} The generated user ID.
+   */
   generateRandomUserId(): number {
-    return Math.floor(10000 + Math.random() * 90000); // 5-digit unique ID
+    return Math.floor(10000 + Math.random() * 90000);
   }
 }
