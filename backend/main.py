68c8563dc05f7c2e9766971033817781fb5c49a8
Andrew <atitoia@wgu.edu>
2024-11-11 08:26:09 -0800

    Added CORS to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,10 @@
+from flask_cors import CORS
 from app import create_app
 from config import config
 import os
 
 if __name__ == "__main__":
-    # Determine the environment and Load the appropriate configuration
     env = os.getenv('FLASK_ENV', 'default')
     app = create_app(config_class=config[env])
-
-    # Run the app on host 0.0.0.0 to be accessible on local network
+    CORS(app)  # Enable CORS globally
     app.run(host="0.0.0.0", port=5000, debug=config[env].DEBUG)
